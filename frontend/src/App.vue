b90c9071099162a476c3d82e6b7172a190d1331b
Kairlec <sunfokairlec@gmail.com>
2022-04-08 01:22:55 +0800

    initial frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import MenuBar from "./components/MenuBar.vue";
+</script>
+
+<template>
+  <el-config-provider :size="size" :z-index="zIndex">
+    <MenuBar></MenuBar>
+<!--    <HelloWorld msg="Hello Vue 3 + Vite"/>-->
+  </el-config-provider>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
+<script lang="ts">
+import {defineComponent} from 'vue'
+import {ElConfigProvider} from 'element-plus'
+
+export default defineComponent({
+  components: {
+    ElConfigProvider,
+  },
+  setup() {
+    return {
+      zIndex: 3000,
+      size: 'small',
+    }
+  },
+})
+</script>
---
+++ frontend/src/components/MenuBar.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {ref} from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+<template>
+  <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect">
+    <el-image src="/src/assets/logo.svg" class="logo"></el-image>
+    <el-menu-item index="1">首页</el-menu-item>
+    <el-menu-item index="2">题目</el-menu-item>
+    <el-menu-item index="3">提交</el-menu-item>
+    <el-menu-item index="4">比赛</el-menu-item>
+    <el-menu-item index="5">排行</el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts">
+export default {
+  name: "MenuBar"
+}
+</script>
+
+<style scoped>
+.el-menu-item {
+  width: 16%;
+}
+.logo{
+  margin-right: 30px;
+}
+</style>
+<style>
+.el-image__inner{
+  max-height: 58px;
+}
+</style>
